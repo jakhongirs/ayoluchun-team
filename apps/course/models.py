404fed80bdc9fc8555b2572379edc67bb9a3100e
jakhongirs <jakhongirsv@gmail.com>
2023-08-09 21:44:59 +0500

    feat: category model in course app

---
+++ apps/course/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="category")
+
+    def __str__(self):
+        return self.name
