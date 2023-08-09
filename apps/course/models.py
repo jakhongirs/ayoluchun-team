5cdfe5f0c1d3491d16e15adef6f82e8dc1a0a4cb
shahzood <shahzod.dv@gmail.com>
2023-08-09 22:16:41 +0500

    fix

--- apps/course/models.py
+++ apps/course/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to="category")
+    rasm = models.ImageField(upload_to="category")
 
     def __str__(self):
         return self.name
