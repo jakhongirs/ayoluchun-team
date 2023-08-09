12b9aa6ea285c4dcc762c9ea0d7f072f253182c0
jakhongirs <jakhongirsv@gmail.com>
2023-08-09 22:18:02 +0500

    test conflicts

--- apps/course/models.py
+++ apps/course/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    rasm = models.ImageField(upload_to="category")
+    avatar = models.ImageField(upload_to="category")
 
     def __str__(self):
         return self.name
